edc9355716675a7fb01817b328f480963be3763b
ryoma <dara.ryouma@gmail.com>
2019-05-10 18:04:41 +0900

    resolut

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,8 +7,8 @@ v-layout#Home(fill-height column)
                     v-layout(justify-center)
                         v-dialog(v-model='dialog' presistent max-width='600px')
                             template(v-slot:activator='{ on }')
-                                v-card.orange.lighten-3.ma-2(width='15vw' :aspect-ratio='9/16')
-                                    v-card-text(v-on='on') 教卓
+                                v-card.orange.lighten-3.ma-2(v-on='on' width='15vw' :aspect-ratio='9/16')
+                                    v-card-text 教卓
                             //- dialog card information
                             v-card
                                 v-card-title
@@ -21,11 +21,11 @@ v-layout#Home(fill-height column)
                                             v-flex(xs12 sm12 md12)
                                                 v-text-field(label='position-number' v-model='res_pos' required)
                                             v-flex(xs12 sm12 md12)
-                                                v-text-field(label='password' v-model='res_pass' required)
-                                    v-card-actions
-                                        v-btn(color='blue darken-1' flat @click='onSend') Send
-                                    v-card-actions
-                                        v-btn(color='blue darken-1' flat @click='false') Cancel
+                                                v-text-field(label='password' v-model='res_pass' required) {{ maskMsg() }}
+                                            v-card-actions(xs12 sm8 md6)
+                                                v-btn(color='blue darken-1' flat @click='onSend') Send
+                                            v-card-actions(xs12 sm4 md6)
+                                                v-btn(color='blue darken-1' flat @click='false') Cancel
                     v-layout(justify-center column)
                         v-flex
                             v-layout(row wrap align-center justify-space-around)
@@ -33,11 +33,6 @@ v-layout#Home(fill-height column)
                                     template(v-slot:activator='{ on }')
                                         v-card.primary.lighten-4.mb-2(v-for='i in num' :key='i' width='calc(100vw/7.5)' :aspect-ratio='1')
                                             v-card-text(canter) {{ i }}
-
-
-    p hoge {{this.hoge}}
-    //- p huga {{this.huga}}
-    p member {{this.member}}
 
 </template>
 
@@ -53,19 +48,16 @@ import io from 'socket.io-client';
 })
 export default class Home extends Vue {
 
-    protected hoge = 0;
-    protected huga = 0;
-    protected an = 0 ;
     protected dialog = false;
-    protected text = '';
     protected num = [] as string[];
-    protected position = 0;
     protected socket = io();
     protected res_anum = 0;
     protected res_pos = 0;
-    protected res_pass = 0;
-    protected msg = '';
-
+    protected res_pass = '';
+    protected msg = {
+        pre: '',
+    };
+    // 初期状態
     protected created() {
         for (let i = 0; i < 43; i++) {
             this.num[i] = String(i + 1);
@@ -81,7 +73,7 @@ export default class Home extends Vue {
     }
 
     protected onSend() {
-        if (this.res_anum === 0 || this.res_pos === 0 || this.res_pass === 0) {
+        if (this.res_anum === 0 || this.res_pos === 0 || this.res_pass === '') {
             return;
         }
 
@@ -91,25 +83,28 @@ export default class Home extends Vue {
             passward: this.res_pass,
         };
         this.socket.emit('res_seat', JSON.stringify(myJson));
+        [ this.res_pass, this.msg.pre ] = ['' , ''];
+        this.res_anum = 0;
+        this.res_pos = 0;
+        this.dialog = false;
     }
+
     protected insertNum(name: string, pos: number ) {
         // for (let i = 0; i < 43; i++) {
         //     this.num[i] = (`${i + 1}`);
         // }
-
         this.num[pos] = name;
     }
 
-    // protected onClick(i: number) {
-    //     this.res_ = i;
-    // }
-    protected change(member: number, hoge: number) {
-        this.huga = hoge;
-        hoge = member;
-        member = this.huga;
-        this.dialog = false;
-    }
+    protected maskMsg() {
+        const char = this.res_pass.charAt(this.res_pass.length - 1);
 
+        // 複数回呼ばれるため一文字に対して一回だけ
+        if (char !== '*') {
+            this.msg.pre += char;   // 末尾にマスク前の文字列を追加
+            this.res_pass = this.res_pass.replace(/\S/g, '*');  // 全ての文字を置き換える
+        }
+    }
 }
 </script>
 
